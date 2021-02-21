9dc1e3de067d445471b85cbf723c966ae4f42ccc
三尧 <statssun@gmail.com>
2021-02-21 15:01:22 +0800

    update

---
+++ scripts/config/webpack.common.js
@@ -0,0 +1,117 @@
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { isDev, PROJECT_PATH } = require('../constant')
+
+module.exports = {
+  entry: {
+    app: resolve(PROJECT_PATH, './src/app.js'),
+  },
+  output: {
+    filename: `js/[name]${isDev ? '' : '.[hash:8]'}.js`,
+    path: resolve(PROJECT_PATH, './dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(PROJECT_PATH, './public/index.html'),
+      filename: 'index.html',
+      cache: false,
+      minify: isDev ? false : {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        removeComments: true,
+        collapseBooleanAttributes: true,
+        collapseInlineTagWhitespace: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        minifyCSS: true,
+        minifyJS: true,
+        minifyURLs: true,
+        useShortDoctype: true,
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false, // 默认就是 false, 若要开启，可在官网具体查看可配置项
+              sourceMap: isDev, // 开启后与 devtool 设置一致, 开发环境开启，生产环境关闭
+              importLoaders: 0, // 指定在 CSS loader 处理前使用的 laoder 数量
+            },
+          },
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false,
+              sourceMap: isDev,
+              importLoaders: 1, // 需要先被 less-loader 处理，所以这里设置为 1
+            },
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              sourceMap: isDev,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false,
+              sourceMap: isDev,
+              importLoaders: 1, // 需要先被 sass-loader 处理，所以这里设置为 1
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDev,
+            },
+          },
+        ],
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10 * 1024,
+              name: '[name].[contenthash:8].[ext]',
+              outputPath: 'assets/images',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot|otf)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[name].[contenthash:8].[ext]',
+              outputPath: 'assets/fonts',
+            },
+          },
+        ],
+      },
+    ],
+  },
+}
